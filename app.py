9f9bb6766cef624d9132337a54f7952b1c11ec8a
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-08-21 19:01:36 -0300

    Adding tests & Updating create endpoint

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def create_task():
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
@@ -43,7 +43,7 @@ def update_task(id):
         if t.id == id:
             task = t
             break
-        
+
     print(task)
     if task == None:
         return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
