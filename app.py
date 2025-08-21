1fce7fda3c4ca60b4f904dfa0d560574b44f5cd0
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-08-21 15:24:38 -0300

    Implemeting CRUD of tasks.

--- app.py
+++ app.py
@@ -1,15 +1,73 @@
 # O nome do arquivo precisa ser app.py
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+tasks = []
+task_id_control = 1
 
-@app.route('/about')
-def about():
-    return "This is the about page."
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control # com isso, conseguimos ter acesso à variável global
+    data = request.get_json() # o request é fornecido pelo Flask e permite acessar os dados da requisição
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list=[task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }    
+
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+        
+    print(task)
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})       
+
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break # Por questões de performance.  Ou seja, se encontrou, sai do loop e não tenta processar os registros restantes da lista.
+    
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify({"message:": "Tarefa deletada com sucesso"})
 
 if __name__ == '__main__': # quando executamos de forma manual o nosso arquivo, o __name__ é igual a __main__
     app.run(debug=True) #este parâmetro vai permitir que vejemos diversas informações de debug no terminal.  Apenas recomendado para desenvolvimento.
